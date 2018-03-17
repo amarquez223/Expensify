bb99a2c6991aa98c7d3ca02cb0859617392efdfd
anderson <anderson@debian.amrconsulting.com>
2018-03-17 18:42:29 -0500

    Adiciona ventana modal con forma para cración de nuevo gasto

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -2,16 +2,19 @@ class ExpensesController < ApplicationController
 	def index
 		@types = Type.all
 		@categories = Category.all
+
 		if params[:type].present?
 			type = params[:type]
 		else
 			type = nil
 		end
+		
 		if params[:cat].present?
 			cat = params[:cat]
 		else
 			cat = nil
 		end
+		
 		if params[:date].present?
 			date = params[:date].to_date
 		else
@@ -21,5 +24,16 @@ class ExpensesController < ApplicationController
 		@transactions = Transaction.expenses_month(date.year,date.month,type,cat,1).order('date DESC')
 
 		render locals: {active_type: type, active_cat: cat, active_date: date}
+	end
+
+	def create
+		@transaction = Transaction.new(transaction_params)
+		@transaction.save
+		redirect_to expenses_path
+	end
+
+	protected
+	def transaction_params
+		params.permit(:type_id,:category_id,:date,:concept,:amount)
 	end
 end
